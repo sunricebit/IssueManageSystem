a6b49a137854e09a0b4dda43f1f4d1b90f785fc8
key <tunahe140525@fpt.edu.vn>
2023-11-28 18:13:22 +0700

    ui: forgot password

---
+++ ViewModels/Auth/ForgotPasswordViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace IMS.ViewModels.Auth
+{
+	public class ForgotPasswordViewModel
+	{
+        [Required(ErrorMessage = "Please enter your email address.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
+
