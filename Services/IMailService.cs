6e6e1799c8e29a272f0ce19afaa66c130846ca5c
key <tunahe140525@fpt.edu.vn>
2023-11-28 09:20:08 +0700

    feat: mailing service

---
+++ Services/IMailService.cs
@@ -0,0 +1,11 @@
+﻿namespace IMS.Services
+{
+    public interface IMailService
+    {
+        public string? GetDomain(string email);
+        public string? GetAddress(string email);
+        public void SendMailConfirm(string email, string hash);
+        public void SendResetPassword(string email, string hash);
+        public void SendPassword(string email, string password);
+    }
+}
