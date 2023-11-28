3179e5cda4112ec28d0bcd3eeb0052da20e86300
sunricebit <vuducduynb2002@gmail.com>
2023-11-28 10:26:27 +0700

    commit authorize

---
+++ Common/CustomAuthozire.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using MySqlX.XDevAPI;
+using System.Net;
+using System.Web.Mvc;
+
+namespace IMS.Common
+{
+    public class CustomAuthorize
+    {
+        private readonly string[] allowedroles;
+
+        public CustomAuthorize(params string[] roles) => allowedroles = roles;
+
+        public bool Authorize(HttpContext context)
+        {
+            string role = context.Session.GetString("role");
+            if (!string.IsNullOrEmpty(role))
+            {
+                if (allowedroles.Contains(role))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IMS.Controllers
+{
+    [Route("Error")]
+    public class ErrorController : Controller
+    {
+        [Route("NotAccess")]
+        public IActionResult NotAccess()
+        {
+            return View();
+        }
+    }
+}
