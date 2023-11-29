2b3db2878e66c74c6b21ae1e36dbf5fb92cf0be8
May Nguyen <may@coinhe.io>
2023-11-29 15:45:28 +0700

    commit landing page

--- Program.cs
+++ Program.cs
@@ -24,6 +24,11 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
 app.MapControllers();
-
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
 app.Run();
 
