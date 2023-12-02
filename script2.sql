f6d1139955068372ef39fc5989a8bc26fab0f453
key <tunahe140525@fpt.edu.vn>
2023-12-02 18:28:59 +0700

    model: scaffolding

---
+++ script2.sql
@@ -0,0 +1,298 @@
+﻿DROP SCHEMA IF EXISTS IMS;
+
+CREATE DATABASE IMS;
+
+USE IMS;
+
+-- CreateTable
+CREATE TABLE `Setting` (
+    `Id` INTEGER NOT NULL AUTO_INCREMENT,
+    `Type` VARCHAR(20) NOT NULL,
+    `Value` VARCHAR(50) NOT NULL,
+    `Description` VARCHAR(400) NULL,
+
+    INDEX `Setting_Type_Value_idx`(`Type`, `Value`),
+    INDEX `Setting_Id_idx`(`Id`),
+    UNIQUE INDEX `Setting_Type_Value_key`(`Type`, `Value`),
+    PRIMARY KEY (`Id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Permission` (
+    `Id` INTEGER NOT NULL AUTO_INCREMENT,
+    `RoleId` INTEGER NOT NULL,
+    `Page` VARCHAR(191) NOT NULL,
+    `CanCreate` BOOLEAN NOT NULL DEFAULT false,
+    `CanRead` BOOLEAN NOT NULL DEFAULT false,
+    `CanUpdate` BOOLEAN NOT NULL DEFAULT false,
+    `CanDelete` BOOLEAN NOT NULL DEFAULT false,
+
+    UNIQUE INDEX `Permission_RoleId_key`(`RoleId`),
+    INDEX `Permission_RoleId_Page_idx`(`RoleId`, `Page`),
+    UNIQUE INDEX `Permission_RoleId_Page_key`(`RoleId`, `Page`),
+    PRIMARY KEY (`Id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Contact` (
+    `Id` INTEGER NOT NULL AUTO_INCREMENT,
+    `Email` VARCHAR(100) NOT NULL,
+    `Name` VARCHAR(100) NOT NULL,
+    `Phone` VARCHAR(15) NULL,
+    `IsValid` BOOLEAN NOT NULL DEFAULT true,
+    `CreatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `CarerId` INTEGER NOT NULL,
+    `ContactTypeId` INTEGER NOT NULL,
+
+    INDEX `Contact_Id_idx`(`Id`),
+    PRIMARY KEY (`Id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Message` (
+    `Id` INTEGER NOT NULL AUTO_INCREMENT,
+    `Content` VARCHAR(100) NOT NULL,
+    `ContactId` INTEGER NULL,
+
+    INDEX `Message_Id_idx`(`Id`),
+    PRIMARY KEY (`Id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Post` (
+    `Id` INTEGER NOT NULL AUTO_INCREMENT,
+    `Title` TEXT NOT NULL,
+    `Description` TEXT NULL,
+    `CreatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `UpdatedAt` DATETIME(3) NULL,
+    `IsPublic` BOOLEAN NOT NULL DEFAULT false,
+    `ImageUrl` VARCHAR(100) NULL,
+    `UserId` INTEGER NOT NULL,
+    `CategoryId` INTEGER NULL,
+
+    INDEX `Post_Id_idx`(`Id`),
+    PRIMARY KEY (`Id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `User` (
+    `Id` INTEGER NOT NULL AUTO_INCREMENT,
+    `Email` VARCHAR(100) NOT NULL,
+    `Password` VARCHAR(64) NOT NULL,
+    `RoleId` INTEGER NOT NULL,
+    `Name` VARCHAR(50) NOT NULL,
+    `Avatar` VARCHAR(100) NULL,
+    `Gender` BOOLEAN NULL,
+    `Phone` VARCHAR(15) NULL,
+    `Address` VARCHAR(191) NULL,
+    `Status` BOOLEAN NULL,
+    `LstAccessTime` DATETIME(3) NULL,
+    `ConfirmToken` VARCHAR(64) NULL,
+    `ResetToken` VARCHAR(64) NULL,
+
+    INDEX `User_Email_idx`(`Email`),
+    INDEX `User_Id_idx`(`Id`),
+    UNIQUE INDEX `User_Email_key`(`Email`),
+    PRIMARY KEY (`Id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Subject` (
+    `Id` INTEGER NOT NULL AUTO_INCREMENT,
+    `Code` VARCHAR(10) NOT NULL,
+    `Name` VARCHAR(100) NOT NULL,
+    `Description` TEXT NULL,
+    `IsActive` BOOLEAN NOT NULL DEFAULT true,
+
+    INDEX `Subject_Id_idx`(`Id`),
+    PRIMARY KEY (`Id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Assignment` (
+    `Id` INTEGER NOT NULL AUTO_INCREMENT,
+    `Code` VARCHAR(50) NOT NULL,
+    `Name` VARCHAR(100) NOT NULL,
+    `Description` TEXT NULL,
+    `SubjectId` INTEGER NULL,
+
+    INDEX `Assignment_Id_idx`(`Id`),
+    PRIMARY KEY (`Id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Class` (
+    `Id` INTEGER NOT NULL AUTO_INCREMENT,
+    `Name` VARCHAR(15) NOT NULL,
+    `Description` VARCHAR(300) NULL,
+    `TeacherId` INTEGER NULL,
+    `SubjectId` INTEGER NULL,
+
+    INDEX `Class_Id_idx`(`Id`),
+    PRIMARY KEY (`Id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `ClassStudent` (
+    `StudentId` INTEGER NOT NULL,
+    `ClassId` INTEGER NOT NULL,
+
+    INDEX `ClassStudent_ClassId_StudentId_idx`(`ClassId`, `StudentId`),
+    PRIMARY KEY (`ClassId`, `StudentId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Project` (
+    `Id` INTEGER NOT NULL AUTO_INCREMENT,
+    `EnglishName` VARCHAR(100) NOT NULL,
+    `VietnameseName` VARCHAR(100) NOT NULL,
+    `Status` BOOLEAN NULL DEFAULT true,
+    `Description` TEXT NOT NULL,
+    `ClassId` INTEGER NULL,
+    `LeaderId` INTEGER NULL,
+
+    INDEX `Project_Id_idx`(`Id`),
+    PRIMARY KEY (`Id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `ProjectStudent` (
+    `StudentId` INTEGER NOT NULL,
+    `ProjectId` INTEGER NOT NULL,
+
+    INDEX `ProjectStudent_ProjectId_StudentId_idx`(`ProjectId`, `StudentId`),
+    PRIMARY KEY (`ProjectId`, `StudentId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Issue` (
+    `Id` INTEGER NOT NULL AUTO_INCREMENT,
+    `Title` TEXT NOT NULL,
+    `Description` TEXT NOT NULL,
+    `MilestoneId` INTEGER NULL,
+    `ProjectId` INTEGER NULL,
+    `AuthorId` INTEGER NOT NULL,
+    `AssigneeId` INTEGER NULL,
+
+    INDEX `Issue_Id_idx`(`Id`),
+    PRIMARY KEY (`Id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `IssueSetting` (
+    `Id` INTEGER NOT NULL AUTO_INCREMENT,
+    `Name` VARCHAR(50) NOT NULL,
+    `Status` BOOLEAN NOT NULL DEFAULT true,
+    `ClassId` INTEGER NULL,
+    `IssueId` INTEGER NULL,
+    `ProjectId` INTEGER NULL,
+
+    INDEX `IssueSetting_Id_idx`(`Id`),
+    PRIMARY KEY (`Id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Milestone` (
+    `Id` INTEGER NOT NULL AUTO_INCREMENT,
+    `Title` TEXT NOT NULL,
+    `Description` TEXT NULL,
+    `StartDate` DATETIME(3) NULL,
+    `EndDate` DATETIME(3) NULL,
+    `ClassId` INTEGER NULL,
+    `AssignmentId` INTEGER NULL,
+
+    INDEX `Milestone_Id_idx`(`Id`),
+    PRIMARY KEY (`Id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Permission` ADD CONSTRAINT `Permission_RoleId_fkey` FOREIGN KEY (`RoleId`) REFERENCES `Setting`(`Id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Contact` ADD CONSTRAINT `Contact_ContactTypeId_fkey` FOREIGN KEY (`ContactTypeId`) REFERENCES `Setting`(`Id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Contact` ADD CONSTRAINT `Contact_CarerId_fkey` FOREIGN KEY (`CarerId`) REFERENCES `User`(`Id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Message` ADD CONSTRAINT `Message_ContactId_fkey` FOREIGN KEY (`ContactId`) REFERENCES `Contact`(`Id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Post` ADD CONSTRAINT `Post_UserId_fkey` FOREIGN KEY (`UserId`) REFERENCES `User`(`Id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Post` ADD CONSTRAINT `Post_CategoryId_fkey` FOREIGN KEY (`CategoryId`) REFERENCES `Setting`(`Id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `User` ADD CONSTRAINT `User_RoleId_fkey` FOREIGN KEY (`RoleId`) REFERENCES `Setting`(`Id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Assignment` ADD CONSTRAINT `Assignment_SubjectId_fkey` FOREIGN KEY (`SubjectId`) REFERENCES `Subject`(`Id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Class` ADD CONSTRAINT `Class_SubjectId_fkey` FOREIGN KEY (`SubjectId`) REFERENCES `Subject`(`Id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Class` ADD CONSTRAINT `Class_TeacherId_fkey` FOREIGN KEY (`TeacherId`) REFERENCES `User`(`Id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `ClassStudent` ADD CONSTRAINT `ClassStudent_ClassId_fkey` FOREIGN KEY (`ClassId`) REFERENCES `Class`(`Id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `ClassStudent` ADD CONSTRAINT `ClassStudent_StudentId_fkey` FOREIGN KEY (`StudentId`) REFERENCES `User`(`Id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Project` ADD CONSTRAINT `Project_ClassId_fkey` FOREIGN KEY (`ClassId`) REFERENCES `Class`(`Id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Project` ADD CONSTRAINT `Project_LeaderId_fkey` FOREIGN KEY (`LeaderId`) REFERENCES `User`(`Id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `ProjectStudent` ADD CONSTRAINT `ProjectStudent_ProjectId_fkey` FOREIGN KEY (`ProjectId`) REFERENCES `Project`(`Id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `ProjectStudent` ADD CONSTRAINT `ProjectStudent_StudentId_fkey` FOREIGN KEY (`StudentId`) REFERENCES `User`(`Id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Issue` ADD CONSTRAINT `Issue_MilestoneId_fkey` FOREIGN KEY (`MilestoneId`) REFERENCES `Milestone`(`Id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Issue` ADD CONSTRAINT `Issue_ProjectId_fkey` FOREIGN KEY (`ProjectId`) REFERENCES `Project`(`Id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Issue` ADD CONSTRAINT `Issue_AuthorId_fkey` FOREIGN KEY (`AuthorId`) REFERENCES `User`(`Id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Issue` ADD CONSTRAINT `Issue_AssigneeId_fkey` FOREIGN KEY (`AssigneeId`) REFERENCES `User`(`Id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `IssueSetting` ADD CONSTRAINT `IssueSetting_ClassId_fkey` FOREIGN KEY (`ClassId`) REFERENCES `Class`(`Id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `IssueSetting` ADD CONSTRAINT `IssueSetting_IssueId_fkey` FOREIGN KEY (`IssueId`) REFERENCES `Issue`(`Id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `IssueSetting` ADD CONSTRAINT `IssueSetting_ProjectId_fkey` FOREIGN KEY (`ProjectId`) REFERENCES `Project`(`Id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Milestone` ADD CONSTRAINT `Milestone_ClassId_fkey` FOREIGN KEY (`ClassId`) REFERENCES `Class`(`Id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Milestone` ADD CONSTRAINT `Milestone_AssignmentId_fkey` FOREIGN KEY (`AssignmentId`) REFERENCES `Assignment`(`Id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+INSERT INTO `IMS`.`Setting` (`Type`, `Value`)
+VALUES ('ROLE', 'Admin'),
+       ('ROLE', 'Marketer'),
+       ('ROLE', 'Teacher'),
+       ('ROLE', 'Student'),
+       ('POST_CATEGORY', 'Art and Culture'),
+       ('POST_CATEGORY', 'Travel and Adventure'),
+       ('POST_CATEGORY', 'Science and Technology'),
+       ('POST_CATEGORY', 'Health and Lifestyle'),
+       ('POST_CATEGORY', 'Learning and Personal Development'),
+       ('POST_CATEGORY', 'Family Life'),
+       ('CONTACT_TYPE', 'Networking'),
+       ('CONTACT_TYPE', 'IT Career Development'),
+       ('CONTACT_TYPE', 'Financial Aid and Scholarships'),
+       ('CONTACT_TYPE', 'Faculty and Research'),
+       ('CONTACT_TYPE', 'International Students');
