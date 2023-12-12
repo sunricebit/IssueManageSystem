ae89c8825191c33aeb7373b985b3a7ec2cdc1224
May Nguyen <may@coinhe.io>
2023-12-12 17:16:50 +0700

    commit Blog

--- DAO/PostDAO.cs
+++ DAO/PostDAO.cs
@@ -2,11 +2,14 @@
 {
     public partial class PostDAO
     {
+        public int? Id { get; set; } = null!;
         public string Title { get; set; } = null!;
         public string? Description { get; set; }
         public string? ImageUrl { get; set; }
-        public string AuthorName { get; set; }
+        public string? Excerpt { get; set; }
+        public string? AuthorName { get; set; }
         public int? CategoryId { get; set; }
+        public string? CategoryName { get; set; }
         public DateTime CreatedAt { get; set; }
 
     }
